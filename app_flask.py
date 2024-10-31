aad9cd365e41ca3500c731943854167f74a55d96
fanding <fanding@cn.ibm.com>
2024-10-31 10:53:36 +0800

    update page

--- app_flask.py
+++ app_flask.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-    return "hello world!"
+    # return "hello world!"
+    return render_template('hello.html')
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', port=8080)
+    # app.run(debug=True)
